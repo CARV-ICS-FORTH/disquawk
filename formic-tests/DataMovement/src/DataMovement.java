82fa3ebc5fa3de2e27d88fb70bc09e11a43ade76
Foivos S. Zakkak <foivos@zakkak.net>
2014-07-04 20:37:59 +0300

    Fix in softcache and extra cases in DataMovement test

--- formic-tests/DataMovement/src/DataMovement.java
+++ formic-tests/DataMovement/src/DataMovement.java
@@ -38,14 +38,14 @@ public class DataMovement {
 		Integer temp2 = (Integer)Address.fromPrimitive(addr2).toObject();
 
 		System.out.print("Object transfer...");
-		if (temp2.intValue() == temp.intValue())
+		if (temp2.intValue() == temp.intValue()) {
 			System.out.println("[1;32mPASS[0m");
-		else {
+		} else {
 			System.out.println("[1;31mFAIL[0m");
 			System.out.println("local = "+temp+
-			                   "&local = "+Integer.toHexString(addr));
+			                   " &local = "+Integer.toHexString(addr));
 			System.out.println("remote = "+temp2+
-			                   "&remote = "+Integer.toHexString(addr2));
+			                   " &remote = "+Integer.toHexString(addr2));
 		}
 
 		// Now check arrays
@@ -67,14 +67,14 @@ public class DataMovement {
 		Integer[] arr2 = (Integer[])Address.fromPrimitive(addr2).toObject();
 
 		System.out.print("Array transfer...");
-		if (arr2[1].intValue() == arr[1].intValue())
+		if (arr2[1].intValue() == arr[1].intValue()) {
 			System.out.println("[1;32mPASS[0m");
-		else {
+		} else {
 			System.out.println("[1;31mFAIL[0m");
 			System.out.println("local = "+arr[1]+
-			                   "&local = "+Integer.toHexString(addr));
+			                   " &local = "+Integer.toHexString(addr));
 			System.out.println("remote = "+arr2[1]+
-			                   "&remote = "+Integer.toHexString(addr2));
+			                   " &remote = "+Integer.toHexString(addr2));
 		}
 
 		// Now check large arrays
@@ -102,9 +102,72 @@ public class DataMovement {
 		} else {
 			System.out.println("[1;31mFAIL[0m");
 			System.out.println("local = "+larr[1024]+
-			                   "&local = "+Integer.toHexString(addr));
+			                   " &local = "+Integer.toHexString(addr));
 			System.out.println("remote = "+larr2[1024]+
-			                   "&remote = "+Integer.toHexString(addr2));
+			                   " &remote = "+Integer.toHexString(addr2));
+		}
+
+		// Now arrays of Strings
+
+		String[] arrS  = new String[4];
+		arrS[0] = new String("1234");
+		arrS[1] = new String("1234567");
+		arrS[2] = new String("123456");
+		arrS[3] = new String("123456789");
+		addr   = Address.fromObject(arrS).toUWord().toPrimitive();
+
+		// Add some delay to make sure the write is performed on both
+		// nodes
+		for(int i=0; i<20000; ++i) {
+		}
+
+		bid   = ((addr >>> 27) == 1) ? 0 : 1;
+		addr2 = (bid << 27) + (addr & 0x7FFFFFF);
+		String[] arrS2 = (String[])Address.fromPrimitive(addr2).toObject();
+
+		System.out.print("String Array transfer...");
+		if (arrS2[1].equals(arrS[1])) {
+			System.out.println("[1;32mPASS[0m");
+		} else {
+			System.out.println("[1;31mFAIL[0m");
+			System.out.println("local = "+arrS[1]+
+			                   " &local = "+Integer.toHexString(addr));
+			System.out.println("remote = "+arrS2[1]+
+			                   " &remote = "+Integer.toHexString(addr2));
+		}
+
+		// Now check 2D arrays
+
+		Integer[][] arrdd = new Integer[8][8];
+		for (int i=0; i<8; ++i) {
+			for (int j=0; j<8; ++j) {
+				arrdd[i][j] = new Integer((i+1)*(j+1));
+			}
+		}
+		addr = Address.fromObject(arrdd).toUWord().toPrimitive();
+
+		// Add some delay to make sure the write is performed on both
+		// nodes
+		for(int i=0; i<20000; ++i) {
+		}
+
+		bid   = ((addr >>> 27) == 1) ? 0 : 1;
+		addr2 = (bid << 27) + (addr & 0x7FFFFFF);
+		Integer[][] arrdd2 = (Integer[][])Address.fromPrimitive(addr2).toObject();
+
+		System.out.print("2D array transfer...");
+		if (arrdd2[7][3].intValue() == arrdd[7][3].intValue()) {
+			System.out.println("[1;32mPASS[0m");
+			System.out.println("local = "+arrdd[7][3]+
+			                   " &local = "+Integer.toHexString(addr));
+			System.out.println("remote = "+arrdd2[7][3]+
+			                   " &remote = "+Integer.toHexString(addr2));
+		} else {
+			System.out.println("[1;31mFAIL[0m");
+			System.out.println("local = "+arrdd[7][3]+
+			                   " &local = "+Integer.toHexString(addr));
+			System.out.println("remote = "+arrdd2[7][3]+
+			                   " &remote = "+Integer.toHexString(addr2));
 		}
 
 	}
