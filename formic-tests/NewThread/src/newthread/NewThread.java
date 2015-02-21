41e2c5e3318322ca0451ee8076edec38e8cca070
Foivos S. Zakkak <foivos@zakkak.net>
2015-02-21 11:34:17 +0200

    Update NewThread test

--- formic-tests/NewThread/src/newthread/NewThread.java
+++ formic-tests/NewThread/src/newthread/NewThread.java
@@ -10,17 +10,17 @@ import java.lang.Thread;
 
 public class NewThread {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		Hello task = new Hello();
-		Thread t1 = new Thread(task);
-		Thread t2 = new Thread(task);
-		Thread t3 = new Thread(task);
-		Thread t4 = new Thread(task);
-		Thread t5 = new Thread(task);
-		Thread t6 = new Thread(task);
-		Thread t7 = new Thread(task);
-		Thread t8 = new Thread(task);
+		Hello  task = new Hello();
+		Thread t1   = new Thread(task);
+		Thread t2   = new Thread(task);
+		Thread t3   = new Thread(task);
+		Thread t4   = new Thread(task);
+		Thread t5   = new Thread(task);
+		Thread t6   = new Thread(task);
+		Thread t7   = new Thread(task);
+		Thread t8   = new Thread(task);
 
 		t1.start();
 		t2.start();
@@ -31,14 +31,17 @@ public class NewThread {
 		t7.start();
 		t8.start();
 		// FIXME: join is not ready yet
-		//t1.join();
-		//t2.join();
-		//t3.join();
-		//t4.join();
-		//t5.join();
-		//t6.join();
-		//t7.join();
-		//t8.join();
+		t1.join();
+		t2.join();
+		t3.join();
+		t4.join();
+		t5.join();
+		t6.join();
+		t7.join();
+		t8.join();
+
+		System.out.println(
+			"I am done (This should be printed after the Hello messages)");
 		while(true);
 	}
 
