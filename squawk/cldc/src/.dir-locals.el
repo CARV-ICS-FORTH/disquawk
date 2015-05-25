021fe8aeb5105cc2803edc5ccac87c54f8e99f7d
Foivos S. Zakkak <foivos@zakkak.net>
2015-05-25 20:42:34 +0300

    Add .dir-locals.el

---
+++ squawk/cldc/src/.dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((java-mode
+  (tab-width . 4)
+  (c-basic-offset . 4)
+  (indent-tabs-mode)
+  (eval smart-tabs-mode)
+  ))
